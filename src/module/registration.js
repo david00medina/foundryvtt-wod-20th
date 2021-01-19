5955ad086aa21047880d4543143cb787fca9f006
david00medina <david00medina@hotmail.com>
2021-01-19 16:45:15 +0000

    Added manifest. Deleted TypeScript compiled JS

--- src/module/registration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.registerActorSheets = void 0;
-var vampire_sheet_1 = require("./actor/sheet/vampire-sheet");
-function registerActorSheets() {
-    Actors.registerSheet("vampire", vampire_sheet_1.VampireWOD20THActorSheet);
-}
-exports.registerActorSheets = registerActorSheets;
